3437bbd1c6b6f897a2c56b2ea7f23a48cad22f6a
sumancrest0001 <suman.crest0001@gmail.com>
2020-12-24 20:42:34 +0545

    add some validation logics

--- src/Component/MainContent/MainContent.js
+++ src/Component/MainContent/MainContent.js
@@ -1,16 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 import UserInformation from './UserInformation';
-import MainInformation from './MainInformation';
+// import MainInformation from './MainInformation';
 import './style.scss';
 
-const mainContent = () => (
-  <div>
-    <div>
-      Main content section
-    </div>
-    <UserInformation />
-    <MainInformation />
-  </div>
-);
+class MainContent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 1,
+    };
+  }
 
-export default mainContent;
+  validateEmail = value => {
+    if (!(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/).test(value)) {
+      return 'Please provide a valid email';
+    }
+    return undefined;
+  };
+
+  hasNumber = value => new RegExp(/[0-9]/).test(value);
+
+  hasMixed = value => new RegExp(/[a-z]/).test(value)
+    && new RegExp(/[A-Z]/).test(value)
+
+  hasSpecial = value => new RegExp(/[!#@$%^&*)(+=._-]/).test(value)
+
+  strengthIndicator = value => {
+    let strengths = 0;
+    if (value) {
+      if (value.length > 5) strengths += 1;
+      if (value.length > 8) strengths += 1;
+      if (this.hasNumber(value)) strengths += 1;
+      if (this.hasSpecial(value)) strengths += 1;
+      if (this.hasMixed(value)) strengths += 1;
+    }
+    return strengths;
+  }
+
+  validatePassword = value => {
+    if (!value || (this.strengthIndicator(value) < 3 && value.length < 5)) {
+      return ('Password is too weak.');
+    }
+    return undefined;
+  }
+
+  validateConfirmPassword = (value, values) => {
+    if (value !== values.password) {
+      return 'Confirm password doesn\'t match';
+    }
+    return undefined;
+  }
+
+  changeTab = tabNumber => {
+    this.setState({ step: tabNumber });
+  }
+
+  render() {
+    const { step } = this.state;
+    return (
+      <div className="main-content">
+        <div>
+          <div className="form-tabs">
+            <div role="presentation" onClick={() => this.changeTab(1)} className={step === 1 ? 'active' : null}>User information</div>
+            <div role="presentation" onClick={() => this.changeTab(2)} className={step === 2 ? 'active' : null}>Additional information</div>
+          </div>
+        </div>
+        <UserInformation
+          emailValidator={this.validateEmail}
+          passwordStrengthIndicator={this.strengthIndicator}
+          passwordValidator={this.validatePassword}
+          confirmPasswordValidator={this.validateConfirmPassword}
+          moveToNext={this.changeTab}
+        />
+      </div>
+    );
+  }
+}
+
+
+export default MainContent;
